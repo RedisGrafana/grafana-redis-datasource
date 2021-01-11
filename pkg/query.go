e601c3d95ea878b3c9e1d73d88fb7e77284910a3
Mikhail <47795110+mikhailredis@users.noreply.github.com>
2021-01-10 17:49:45 -0800

    Remove "Unknown command" error from response for custom panels (#125)
    * Remove "Unknown command" error from response for custom panels
    
    * Add Tests coverage to backend build and fix test

--- pkg/query.go
+++ pkg/query.go
@@ -4,7 +4,6 @@ import (
 	"context"
 	"encoding/json"
 	"errors"
-	"fmt"
 
 	"github.com/grafana/grafana-plugin-sdk-go/backend"
 	"github.com/grafana/grafana-plugin-sdk-go/backend/log"
@@ -86,7 +85,7 @@ func query(ctx context.Context, query backend.DataQuery, client redisClient) bac
 		return queryFtInfo(qm, client)
 	default:
 		response := backend.DataResponse{}
-		response.Error = fmt.Errorf("Unknown command")
+		log.DefaultLogger.Error("Query", "Command", qm.Command)
 		return response
 	}
 }
