7b14db840e4cfe725cfab02b71c1c32ae1104a29
Andrei Shamanau <andrei.shamanau@softeq.com>
2021-01-19 13:13:38 +0300

    Fix integration tests.

--- pkg/redis-tmscan_integration_test.go
+++ pkg/redis-tmscan_integration_test.go
@@ -16,22 +16,42 @@ import (
 func TestTMScanIntegration(t *testing.T) {
 	// Types
 	types := map[string]string{
-		"test:string": "string",
-		"test:stream": "stream",
-		"test:set":    "set",
-		"test:list":   "list",
-		"test:float":  "string",
-		"test:hash":   "hash",
+		"test:string":                "string",
+		"test:stream":                "stream",
+		"test:set":                   "set",
+		"test:list":                  "list",
+		"test:float":                 "string",
+		"test:hash":                  "hash",
+		"ts:enqueue:queue:customers": "TSDB-TYPE",
+		"queue:complete":             "stream",
+		"queue:orders":               "stream",
+		"ts:len:queue:complete":      "TSDB-TYPE",
+		"queue:customers":            "stream",
+		"product":                    "string",
+		"ts:enqueue:queue:complete":  "TSDB-TYPE",
+		"ts:enqueue:queue:orders":    "TSDB-TYPE",
+		"ts:len:queue:customers":     "TSDB-TYPE",
+		"ts:len:queue:orders":        "TSDB-TYPE",
 	}
 
 	// Memory
 	memory := map[string]int64{
-		"test:string": int64(59),
-		"test:stream": int64(612),
-		"test:set":    int64(265),
-		"test:list":   int64(140),
-		"test:float":  int64(59),
-		"test:hash":   int64(108),
+		"test:string":                int64(59),
+		"test:stream":                int64(612),
+		"test:set":                   int64(265),
+		"test:list":                  int64(140),
+		"test:float":                 int64(59),
+		"test:hash":                  int64(108),
+		"ts:enqueue:queue:customers": int64(4236),
+		"queue:complete":             int64(12876),
+		"queue:orders":               int64(1219),
+		"ts:len:queue:complete":      int64(4231),
+		"queue:customers":            int64(2035),
+		"product":                    int64(49),
+		"ts:enqueue:queue:complete":  int64(4235),
+		"ts:enqueue:queue:orders":    int64(4233),
+		"ts:len:queue:customers":     int64(4232),
+		"ts:len:queue:orders":        int64(4229),
 	}
 
 	// Client
@@ -39,26 +59,21 @@ func TestTMScanIntegration(t *testing.T) {
 	client := radixV3Impl{radixClient: radixClient}
 
 	// Response
-	resp := queryTMScan(queryModel{Cursor: "0"}, &client)
+	resp := queryTMScan(queryModel{Cursor: "0", Count: 20}, &client)
 	require.Len(t, resp.Frames, 2)
 	require.Len(t, resp.Frames[0].Fields, 3)
 	require.Len(t, resp.Frames[1].Fields, 1)
 	require.Equal(t, 1, resp.Frames[1].Fields[0].Len())
-	require.Equal(t, 6, resp.Frames[0].Fields[0].Len())
-	require.Equal(t, 6, resp.Frames[0].Fields[1].Len())
-	require.Equal(t, 6, resp.Frames[0].Fields[2].Len())
+	require.Equal(t, 16, resp.Frames[0].Fields[0].Len())
+	require.Equal(t, 16, resp.Frames[0].Fields[1].Len())
+	require.Equal(t, 16, resp.Frames[0].Fields[2].Len())
 	require.Equal(t, "0", resp.Frames[1].Fields[0].At(0))
 
 	// Keys
-	keys := []string{
-		resp.Frames[0].Fields[0].At(0).(string),
-		resp.Frames[0].Fields[0].At(1).(string),
-		resp.Frames[0].Fields[0].At(2).(string),
-		resp.Frames[0].Fields[0].At(3).(string),
-		resp.Frames[0].Fields[0].At(4).(string),
-		resp.Frames[0].Fields[0].At(5).(string),
+	keys := []string{}
+	for i := 0; i < resp.Frames[0].Fields[0].Len(); i++ {
+		keys = append(keys, resp.Frames[0].Fields[0].At(i).(string))
 	}
-
 	for i, key := range keys {
 		require.Equal(t, types[key], resp.Frames[0].Fields[1].At(i), "Invalid type returned")
 		require.Equal(t, memory[key], resp.Frames[0].Fields[2].At(i), "Invalid memory size returned")
@@ -80,8 +95,8 @@ func TestTMScanIntegrationWithNoMatched(t *testing.T) {
 	require.Len(t, resp.Frames[1].Fields, 1)
 	require.Equal(t, 1, resp.Frames[1].Fields[0].Len())
 	require.Equal(t, 0, resp.Frames[0].Fields[0].Len())
-	require.Equal(t, 0, resp.Frames[0].Fields[0].Len())
-	require.Equal(t, 0, resp.Frames[0].Fields[0].Len())
+	require.Equal(t, 0, resp.Frames[0].Fields[1].Len())
+	require.Equal(t, 0, resp.Frames[0].Fields[2].Len())
 	require.NotEqual(t, "0", resp.Frames[1].Fields[0].At(0))
 }
 
@@ -94,35 +109,15 @@ func TestTMScanIntegrationWithMatched(t *testing.T) {
 	client := radixV3Impl{radixClient: radixClient}
 
 	// Response
-	resp := queryTMScan(queryModel{Cursor: "0", Match: "test:*"}, &client)
+	resp := queryTMScan(queryModel{Cursor: "0", Match: "test:*", Count: 20}, &client)
 	require.Len(t, resp.Frames, 2)
 	require.Len(t, resp.Frames[0].Fields, 3)
 	require.Len(t, resp.Frames[1].Fields, 1)
 	require.Equal(t, 1, resp.Frames[1].Fields[0].Len())
 	require.Equal(t, 6, resp.Frames[0].Fields[0].Len())
-	require.Equal(t, 6, resp.Frames[0].Fields[0].Len())
-	require.Equal(t, 6, resp.Frames[0].Fields[0].Len())
+	require.Equal(t, 6, resp.Frames[0].Fields[1].Len())
+	require.Equal(t, 6, resp.Frames[0].Fields[2].Len())
 	require.Equal(t, "0", resp.Frames[1].Fields[0].At(0))
-}
-
-/**
- * TMSCAN with Count 1
- */
-func TestTMScanIntegrationWithCount(t *testing.T) {
-	// Client
-	radixClient, _ := radix.NewPool("tcp", fmt.Sprintf("127.0.0.1:%d", integrationTestPort), 10)
-	client := radixV3Impl{radixClient: radixClient}
-
-	// Response
-	resp := queryTMScan(queryModel{Cursor: "0", Count: 1}, &client)
-	require.Len(t, resp.Frames, 2)
-	require.Len(t, resp.Frames[0].Fields, 3)
-	require.Len(t, resp.Frames[1].Fields, 1)
-	require.Equal(t, 1, resp.Frames[1].Fields[0].Len())
-	require.Equal(t, 1, resp.Frames[0].Fields[0].Len())
-	require.Equal(t, 1, resp.Frames[0].Fields[0].Len())
-	require.Equal(t, 1, resp.Frames[0].Fields[0].Len())
-	require.NotEqual(t, "0", resp.Frames[1].Fields[0].At(0))
 }
 
 func TestTMScanIntegrationWithSamples(t *testing.T) {
@@ -134,8 +129,8 @@ func TestTMScanIntegrationWithSamples(t *testing.T) {
 	require.Len(t, resp.Frames[0].Fields, 3)
 	require.Len(t, resp.Frames[1].Fields, 1)
 	require.Equal(t, 1, resp.Frames[1].Fields[0].Len())
-	require.Equal(t, 6, resp.Frames[0].Fields[0].Len())
-	require.Equal(t, 6, resp.Frames[0].Fields[0].Len())
-	require.Equal(t, 6, resp.Frames[0].Fields[0].Len())
-	require.Equal(t, "0", resp.Frames[1].Fields[0].At(0))
+	require.GreaterOrEqual(t, resp.Frames[0].Fields[0].Len(), 10)
+	require.GreaterOrEqual(t, resp.Frames[0].Fields[1].Len(), 10)
+	require.GreaterOrEqual(t, resp.Frames[0].Fields[2].Len(), 10)
+	require.NotEqual(t, "0", resp.Frames[1].Fields[0].At(0))
 }
