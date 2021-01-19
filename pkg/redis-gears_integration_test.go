6b4b247659b10f80da16a2ec97ffd785186cd2ae
Mikhail <mikhail@redislabs.com>
2021-01-18 19:22:39 -0800

    Fix integration

--- pkg/redis-gears_integration_test.go
+++ pkg/redis-gears_integration_test.go
@@ -36,12 +36,11 @@ func TestRgPystatsIntegration(t *testing.T) {
 func TestRgDumpregistrationsIntegration(t *testing.T) {
 	// Client
 	radixClient, _ := radix.NewPool("tcp", fmt.Sprintf("127.0.0.1:%d", integrationTestPort), 10)
-	_ = radixClient.Do(radix.Cmd(nil, "RG.PYEXECUTE", "GB('CommandReader').register(trigger='mytrigger')"))
 	client := radixV3Impl{radixClient: radixClient}
 
 	// Response
 	resp := queryRgDumpregistrations(queryModel{Command: "rg.dumpregistrations"}, &client)
-	require.Len(t, resp.Frames[0].Fields, 11)
+	require.Len(t, resp.Frames[0].Fields, 12)
 	require.Equal(t, "id", resp.Frames[0].Fields[0].Name)
 	require.Equal(t, "reader", resp.Frames[0].Fields[1].Name)
 	require.Equal(t, "desc", resp.Frames[0].Fields[2].Name)
@@ -53,7 +52,7 @@ func TestRgDumpregistrationsIntegration(t *testing.T) {
 	require.Equal(t, "numAborted", resp.Frames[0].Fields[8].Name)
 	require.Equal(t, "lastError", resp.Frames[0].Fields[9].Name)
 	require.Equal(t, "args", resp.Frames[0].Fields[10].Name)
-	require.Equal(t, 1, resp.Frames[0].Fields[0].Len())
+	require.Equal(t, 3, resp.Frames[0].Fields[0].Len())
 	require.Equal(t, 1, resp.Frames[0].Fields[1].Len())
 	require.Equal(t, 1, resp.Frames[0].Fields[2].Len())
 	require.Equal(t, 1, resp.Frames[0].Fields[3].Len())
